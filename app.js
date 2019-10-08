2614b146097b615f69b0e7384798d3403829bf5d
wissal <wissal.el@gmail.com>
2019-10-08 16:34:11 -0700

    Refactor our routes to make the code cleaner

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 //we write it here because we want it to only gets executed once(right after the app starts up)
 const tours = JSON.parse(fs.readFileSync(__dirname + '/dev-data/data/tours-simple.json'));
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
     res.status(200).json({
       status: 'success', 
       results: tours.length,
@@ -17,9 +17,9 @@ app.get('/api/v1/tours', (req, res) => {
         tours
        }
     });
-});
+}
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
     const newId = Math.floor(Math.random() * 10000);
     const newTour = Object.assign({id: newId}, req.body);
 
@@ -36,32 +36,28 @@ app.post('/api/v1/tours', (req, res) => {
         }
     );
 
-})
+}
 
-app.get('/api/v1/tours/:id', (req, res) => {
-      //we use this trick to convert a string into a number(by type coercion):
-      const id = req.params.id * 1;
-      const tour = tours.find(el => el.id === id);
-   
-      if(!tour) {
-         return res.status(404).json({
-             status: 'fail',
-             message: 'Invalid Id'
-         })
+const getTour = (req, res) => {
+    //we use this trick to convert a string into a number(by type coercion):
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+ 
+    if(!tour) {
+       return res.status(404).json({
+           status: 'fail',
+           message: 'Invalid Id'
+       })
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+          tour
       }
-      res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    });
-})
+  });
+}
 
-//Put and Patch are both used to update data the difference between them is that
-//with PUT our app receives the entire new updated object. unlike with PATCH
-//which receives only the properties that should be updated on the Object
-
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
     const id = req.params.id * 1;
     const found = tours.filter(el => el.id === id); 
     if(found.length === 0){
@@ -76,9 +72,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
             tour: '<Updated tour here...>'
         }
     })
-})
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
     const id = req.params.id * 1;
     const found = tours.filter(el => el.id === id); 
     if(found.length === 0){
@@ -91,7 +87,11 @@ app.delete('/api/v1/tours/:id', (req, res) => {
         status: 'success',
         data: null
     });
-})
+};
+
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
