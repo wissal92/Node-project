1b12ae73c8dced83db392e64bbb9aa91ca88f896
wissal <wissal.el@gmail.com>
2019-10-08 22:35:00 -0700

    Refactor our code by cleaning app.js and creating handler routes files for users and tours and cleaning them by putting the controllers functions in their own folders

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const fs = require('fs');
+          //************** EXPRESS CODE  *******************/
+
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-//********* middlewares ***********/
+const app = express();
 
 //this middleware gives us access to data of the body without it req.body would not work
 app.use(express.json());
@@ -20,100 +22,10 @@ app.use((req, res, next) => {
 
 //morgan is a middleware that allow us to log request data in the console
 app.use(morgan('dev'))
-//we write it here because we want it to only gets executed once(right after the app starts up)
-const tours = JSON.parse(fs.readFileSync(__dirname + '/dev-data/data/tours-simple.json'));
 
-//**************** ROUTE HANDLERS *************************/
+//this is where we mount ou routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-const getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-      status: 'success',
-      requestTime: req.requestTime, 
-      results: tours.length,
-      data: {
-        tours
-       }
-    });
-}
 
-const createTour = (req, res) => {
-    const newId = Math.floor(Math.random() * 10000);
-    const newTour = Object.assign({id: newId}, req.body);
-
-    tours.push(newTour);
-    fs.writeFile(__dirname + '/dev-data/data/tours-simple.json',
-        JSON.stringify(tours),
-        err => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    tour: newTour
-                }
-            });
-        }
-    );
-
-}
-
-const getTour = (req, res) => {
-    //we use this trick to convert a string into a number(by type coercion):
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
- 
-    if(!tour) {
-       return res.status(404).json({
-           status: 'fail',
-           message: 'Invalid Id'
-       })
-    }
-    res.status(200).json({
-      status: 'success',
-      data: {
-          tour
-      }
-  });
-}
-
-const updateTour = (req, res) => {
-    const id = req.params.id * 1;
-    const found = tours.filter(el => el.id === id); 
-    if(found.length === 0){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    })
-};
-
-const deleteTour = (req, res) => {
-    const id = req.params.id * 1;
-    const found = tours.filter(el => el.id === id); 
-    if(found.length === 0){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
-};
-
-//**************** ROUTE HANDLERS *************************/
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-//******************* SERVER ***********************/
-const port = 3000;
-app.listen(port, () => {
-    console.log('Server listening on port ' + port);
-});
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+
+const tours = JSON.parse(fs.readFileSync(__dirname + '/../dev-data/data/tours-simple.json'));
+
+exports.getAllTours = (req, res) => {
+    console.log(req.requestTime);
+    res.status(200).json({
+      status: 'success',
+      requestTime: req.requestTime, 
+      results: tours.length,
+      data: {
+        tours
+       }
+    });
+}
+
+exports.createTour = (req, res) => {
+    const newId = Math.floor(Math.random() * 10000);
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(__dirname + '/dev-data/data/tours-simple.json',
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+        }
+    );
+
+}
+
+exports.getTour = (req, res) => {
+    //we use this trick to convert a string into a number(by type coercion):
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+ 
+    if(!tour) {
+       return res.status(404).json({
+           status: 'fail',
+           message: 'Invalid Id'
+       })
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+          tour
+      }
+  });
+}
+
+exports.updateTour = (req, res) => {
+    const id = req.params.id * 1;
+    const found = tours.filter(el => el.id === id); 
+    if(found.length === 0){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    })
+};
+
+exports.deleteTour = (req, res) => {
+    const id = req.params.id * 1;
+    const found = tours.filter(el => el.id === id); 
+    if(found.length === 0){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+};
