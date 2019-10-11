5541fcb3fead01c18bdbc0a59c24a500a6682fef
wissal <wissal.el@gmail.com>
2019-10-10 20:11:23 -0700

    Extend our tourModel Schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,7 +5,28 @@ const tourSchema = new mongoose.Schema({
     name: {
       type: String,
       required: [true, 'A tour must have a name'], //=> we can pass an array if we want to display an error when this field is missing we call it a validator  
-      unique: true
+      unique: true,
+      trim: true
+    },
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a max group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 0
     },
     rating: {
       type: Number,
@@ -14,7 +35,27 @@ const tourSchema = new mongoose.Schema({
     price: {
       type: Number,
       required:[true, 'A tour must have a price'],
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required:[true, 'A tour must have a description']
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required:[true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 });
 
 //Our Model:
