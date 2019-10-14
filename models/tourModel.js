c035958a5e258f59f039cae771e86c167b3c1e29
wissal <wissal.el@gmail.com>
2019-10-14 14:53:35 -0700

    Work with mongoose middlwares: document middlware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 //Our Schema:
 const tourSchema = new mongoose.Schema({
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema({
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -24,7 +26,7 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         default: 4.5
     },
-    ratingsAverage: {
+    ratingsQuantity: {
         type: Number,
         default: 0
     },
@@ -60,6 +62,26 @@ const tourSchema = new mongoose.Schema({
     //we add this object to add schema options like displaying the virtual properties
     toJSON: {virtuals: true},
     toObject: {virtuals: true}
+});
+
+//DOCUMENT MIDDLEWARE: runs befor .save() and .create() we can act on the data before it is saved to the database
+//slug => is a string that we can put in the url based on some strings like the name
+tourSchema.pre('save', function(next){ // => every middleware function has access to next so if we have more that one middlware on the stack we should use next
+    //console.log(this) //=> it is going to point to the current processed document
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+tourSchema.pre('save', function(next){
+    console.log('Will save document...')
+    next();
+});
+
+//post middleware functions are executed after all pre middleware functions are executed
+//that is why we have acces to the finished document:
+tourSchema.post('save', function(doc, next){
+    console.log(doc);
+    next();
 });
 
 //OUR VIRTUAL PROPERTY:
