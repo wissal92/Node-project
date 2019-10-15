90ff94246045f25873610ef0c954351ab316b50d
wissal <wissal.el@gmail.com>
2019-10-15 14:25:45 -0700

    Handle invalid database id by creating a friendly user msg for production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,55 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err  => {
+   const msg = `Invalid ${err.path}: ${err.value}.`;
+   return new AppError(msg, 400);
+}
+
+const sendErrorDev = (err, res) =>{
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        err: err,
+        stack: err.stack
+    })
+}
+
+const sendErrorProd = (err, res) => {
+    //Operational, trusted error: send message to client
+    if(err.isOperational){
+        res.status(err.statusCode).json({ 
+            status: err.status, 
+            message: err.message
+        });
+
+    //Programming or other unknown error: don't leak error  details
+   } else {
+       //1) Log error
+       console.log('ERROR 💥', err);
+       
+       //2)send generic message
+       res.status(500).json({
+           status: 'error',
+           message: 'Somthing went wrong :('
+       })
+   }
+}
+
 module.exports = (err, req, res, next) => {
     //console.log(err.stack);
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
+    
+    if(process.env.NODE_ENV === 'development'){ //=> in development we want to get as much info as we can inlike in production 
+        sendErrorDev(err, res);       
+    } else if(process.env.NODE_ENV === 'production') { // => in production we only want the status and msg
+        //handling invalid database errors by transforming the errors that we get from mongoose into an operational error with a friendly msg
+        let error = {...err};
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+        if(err.name === 'CastError') error = handleCastErrorDB(error);
+
+       sendErrorProd(error, res);
+    }
 }
+
+
