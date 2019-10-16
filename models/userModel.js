da453bf11b96fb2cac3ed9aa439620fb58534981
wissal <wissal.el@gmail.com>
2019-10-15 21:40:43 -0700

    Create a custom validator to check if password and password confirm matche, Secure our passwords by creating a moongoose middleware which uses bcrypt so they can be stored in our db in a hashed format

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -21,9 +22,32 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password']
+        required: [true, 'Please confirm your password'],
+        //add our own custom validator to make sure that password and passwordConfirm match
+        validate:{
+            //THIS ONLY WORKS ON CREATE or SAVE!!! => whenever we want to update a user we should use save so that this validator can work
+            validator: function(el){
+                return el === this.password;
+            },
+            message: 'Passwords are not the same!'
+        }
     }
 })
+
+//ENCRYPT OUR PASSWORDS: => we sould never save passwords in our database in plain text
+
+userSchema.pre('save', async function(next){//=> pre runs between getting the data and saving it into the database
+    if(!this.isModified('password')) return next(); //=> because we want to only hash the password when its created or modified
+
+    //to hash our passwords we will use bcrypt => it is a hashing algorithm that will first salt then hash our passwords
+    //salt: means that a random string will be added to our password so that 2 equal passwords do not generate the same hash 
+    //to use this algorithm we could use bcryptjs package:
+    this.password = await bcrypt.hash(this.password, 12); //=> hash password with cost of 12
+
+    this.passwordConfirm = undefined; //=>because we don't want it to persist in our database after making sure that it matchs with our password we delete it
+
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 
